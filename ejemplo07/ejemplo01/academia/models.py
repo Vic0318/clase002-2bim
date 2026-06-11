20b5799665a427752f5e50b91fd3dce0a82d5224
Vamv2003 <victormvega03@gmail.com>
2026-06-11 12:13:58 -0500

    ultimos cambios

--- ejemplo07/ejemplo01/academia/models.py
+++ ejemplo07/ejemplo01/academia/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
 
@@ -10,4 +12,43 @@ class Estudiante(models.Model):
 
 
     def __str__(self):
-        return f"{self.nombre} - {self.apellido} - {self.cedula} - edad: {self.edad}"
+        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - CI: {self.cedula} - Ciudad/Provincia: {self.obtener_ciudad()} - Edad: {self.edad} - Año de nacimiento: {self.obtener_anio()}"
+
+    def obtener_anio(self):
+        anio_actual = datetime.now().year
+        valor = anio_actual - self.edad
+        return valor
+
+    def obtener_ciudad(self):
+        if not self.cedula or len(self.cedula) < 2:
+            return "Desconocida"
+        
+        codigo = self.cedula[:2]
+        provincias = {
+            "01": "Azuay (Cuenca)",
+            "02": "Bolívar (Guaranda)",
+            "03": "Cañar (Azogues)",
+            "04": "Carchi (Tulcán)",
+            "05": "Cotopaxi (Latacunga)",
+            "06": "Chimborazo (Riobamba)",
+            "07": "El Oro (Machala)",
+            "08": "Esmeraldas (Esmeraldas)",
+            "09": "Guayas (Guayaquil)",
+            "10": "Imbabura (Ibarra)",
+            "11": "Loja (Loja)",
+            "12": "Los Ríos (Babahoyo)",
+            "13": "Manabí (Portoviejo)",
+            "14": "Morona Santiago (Macas)",
+            "15": "Napo (Tena)",
+            "16": "Pastaza (Puyo)",
+            "17": "Pichincha (Quito)",
+            "18": "Tungurahua (Ambato)",
+            "19": "Zamora Chinchipe (Zamora)",
+            "20": "Galápagos (Puerto Baquerizo Moreno)",
+            "21": "Sucumbíos (Nueva Loja)",
+            "22": "Orellana (Francisco de Orellana)",
+            "23": "Santo Domingo de los Tsáchilas (Santo Domingo)",
+            "24": "Santa Elena (Santa Elena)",
+            "30": "Exterior"
+        }
+        return provincias.get(codigo, "Desconocida")
